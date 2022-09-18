08c52242a9b92f2c244fedfb9baf4014805f853d
misanchz98 <102152997+misanchz98@users.noreply.github.com>
2022-09-18 17:41:23 +0200

    definimos los campos deseados en items.py

--- amazon_scraper/amazon_scraper/items.py
+++ amazon_scraper/amazon_scraper/items.py
@@ -8,5 +8,10 @@ import scrapy
 
 class AmazonScraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    title = scrapy.Field() 
+    assessment = scrapy.Field()
+    price = scrapy.Field()
+    RAM_size = scrapy.Field()
+    RAM_type = scrapy.Field()
+    memory_speed = scrapy.Field()
     pass
