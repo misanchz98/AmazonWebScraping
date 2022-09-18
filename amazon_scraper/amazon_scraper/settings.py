6215be4cb2d4645ff6bc156ae36b478561ab4426
misanchz98 <102152997+misanchz98@users.noreply.github.com>
2022-09-18 18:19:07 +0200

    instalamos scrapy-user-agents e incluimos downloader middlewares en settings.py

--- amazon_scraper/amazon_scraper/settings.py
+++ amazon_scraper/amazon_scraper/settings.py
@@ -12,12 +12,10 @@ BOT_NAME = 'amazon_scraper'
 SPIDER_MODULES = ['amazon_scraper.spiders']
 NEWSPIDER_MODULE = 'amazon_scraper.spiders'
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'amazon_scraper (+http://www.yourdomain.com)'
 #USER_AGENT = https://developers.whatismybrowser.com/useragents/explore/software_name/googlebot/
-USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
-
+#USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -56,6 +54,11 @@ ROBOTSTXT_OBEY = True
 #DOWNLOADER_MIDDLEWARES = {
 #    'amazon_scraper.middlewares.AmazonScraperDownloaderMiddleware': 543,
 #}
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
