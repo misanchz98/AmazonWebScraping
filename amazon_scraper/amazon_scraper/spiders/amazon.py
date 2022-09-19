63dde590b96b2b954ff61ff2f687950658a6fbed
misanchz98 <102152997+misanchz98@users.noreply.github.com>
2022-09-19 11:13:46 +0200

    probamos otra vez el funcionamiento del scraper

--- amazon_scraper/amazon_scraper/spiders/amazon.py
+++ amazon_scraper/amazon_scraper/spiders/amazon.py
@@ -4,7 +4,7 @@ from ..items import AmazonScraperItem
 class AmazonSpider(scrapy.Spider):
     name = 'amazon'
     allowed_domains = ['amazon.es']
-    start_urls = ['https://www.amazon.es/s?k=tarjeta+grafica&i=computers&rh=n%3A667049031%2Cp_36%3A1323857031&s=review-rank&dc&ds=v1%3AWaGq4VgCol11wI5HIWNRm%2Fi%2BSqyf0tUcxLYF57b%2F%2FkA&qid=1663515144&rnid=1323854031&ref=sr_st_review-rank']
+    start_urls = ['https://www.amazon.es/s?k=tarjeta+gráfica&i=computers&rh=n%3A667049031%2Cp_36%3A1323857031&s=review-rank&dc&ds=v1%3AWaGq4VgCol11wI5HIWNRm%2Fi%2BSqyf0tUcxLYF57b%2F%2FkA&qid=1663515144&rnid=1323854031&ref=sr_st_review-rank']
 
     def parse(self, response):
 
