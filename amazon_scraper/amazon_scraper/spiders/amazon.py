74936a4f9b3298e83aaee18eea83d626a54faa27
misanchz98 <102152997+misanchz98@users.noreply.github.com>
2022-09-19 10:41:31 +0200

    seguimos intentando guardar cada producto en un objeto

--- amazon_scraper/amazon_scraper/spiders/amazon.py
+++ amazon_scraper/amazon_scraper/spiders/amazon.py
@@ -7,8 +7,6 @@ class AmazonSpider(scrapy.Spider):
     start_urls = ['https://www.amazon.es/s?k=tarjeta+grafica&i=computers&rh=n%3A667049031%2Cp_36%3A1323857031&s=review-rank&dc&ds=v1%3AWaGq4VgCol11wI5HIWNRm%2Fi%2BSqyf0tUcxLYF57b%2F%2FkA&qid=1663515144&rnid=1323854031&ref=sr_st_review-rank']
 
     def parse(self, response):
-        items = AmazonScraperItem()
-
         title = response.css('.a-size-medium').css('::text').extract(),
         assessment = response.css('.aok-align-bottom').css('::text').extract(),
         price = response.css('.a-price-whole').css('::text').extract(),
@@ -17,15 +15,18 @@ class AmazonSpider(scrapy.Spider):
         graphic_card = response.css('.puis-padding-right-small:nth-child(3) .a-text-bold').css('::text').extract(),
         memory_speed = response.css('.puis-padding-right-small:nth-child(4) .a-text-bold').css('::text').extract()
         
-        items['title'] = title
-        items['assessment'] = assessment
-        items['price'] = price
-        items['RAM_size'] = RAM_size
-        items['RAM_type'] = RAM_type
-        items['graphic_card'] = graphic_card
-        items['memory_speed'] = memory_speed
+        for i in zip(title, assessment, price, RAM_size, RAM_type, graphic_card, memory_speed):
+            item = AmazonScraperItem(
+                title = title,
+                assessment = assessment,
+                price = price,
+                RAM_size = RAM_size,
+                RAM_type = RAM_type,
+                graphic_card = graphic_card,
+                memory_speed = memory_speed
+            )
 
-        yield items
+            yield item
 
 
  
