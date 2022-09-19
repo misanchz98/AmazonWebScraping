e74075c077780f585cb41b575e0c1d62d6f777ce
misanchz98 <102152997+misanchz98@users.noreply.github.com>
2022-09-19 10:10:34 +0200

    descomentamos item_pipelines en settings.py

--- amazon_scraper/amazon_scraper/settings.py
+++ amazon_scraper/amazon_scraper/settings.py
@@ -76,9 +76,9 @@ FEED_URI = "items.csv"
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'amazon_scraper.pipelines.AmazonScraperPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'amazon_scraper.pipelines.AmazonScraperPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
