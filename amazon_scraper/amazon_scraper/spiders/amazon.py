22d61000e5ef52b74804094340e915cd01339d61
misanchz98 <102152997+misanchz98@users.noreply.github.com>
2022-09-19 10:48:18 +0200

    conseguimos hacer el xpath y el bucle

--- amazon_scraper/amazon_scraper/spiders/amazon.py
+++ amazon_scraper/amazon_scraper/spiders/amazon.py
@@ -7,23 +7,19 @@ class AmazonSpider(scrapy.Spider):
     start_urls = ['https://www.amazon.es/s?k=tarjeta+grafica&i=computers&rh=n%3A667049031%2Cp_36%3A1323857031&s=review-rank&dc&ds=v1%3AWaGq4VgCol11wI5HIWNRm%2Fi%2BSqyf0tUcxLYF57b%2F%2FkA&qid=1663515144&rnid=1323854031&ref=sr_st_review-rank']
 
     def parse(self, response):
-        title = response.css('.a-size-medium').css('::text').extract(),
-        assessment = response.css('.aok-align-bottom').css('::text').extract(),
-        price = response.css('.a-price-whole').css('::text').extract(),
-        RAM_size = response.css('.puis-padding-right-small:nth-child(1) .a-text-bold').css('::text').extract(),
-        RAM_type = response.css('.puis-padding-right-small:nth-child(2) .a-text-bold').css('::text').extract(),
-        graphic_card = response.css('.puis-padding-right-small:nth-child(3) .a-text-bold').css('::text').extract(),
-        memory_speed = response.css('.puis-padding-right-small:nth-child(4) .a-text-bold').css('::text').extract()
+
+        all_div_products = response.xpath('//div[@class="s-main-slot s-result-list s-search-results sg-row"]')
         
-        for i in zip(title, assessment, price, RAM_size, RAM_type, graphic_card, memory_speed):
+        for i in all_div_products:
+
             item = AmazonScraperItem(
-                title = title,
-                assessment = assessment,
-                price = price,
-                RAM_size = RAM_size,
-                RAM_type = RAM_type,
-                graphic_card = graphic_card,
-                memory_speed = memory_speed
+                title = i.css('.a-size-medium').css('::text').extract(),
+                assessment = i.css('.aok-align-bottom').css('::text').extract(),
+                price = i.css('.a-price-whole').css('::text').extract(),
+                RAM_size = i.css('.puis-padding-right-small:nth-child(1) .a-text-bold').css('::text').extract(),
+                RAM_type = i.css('.puis-padding-right-small:nth-child(2) .a-text-bold').css('::text').extract(),
+                graphic_card = i.css('.puis-padding-right-small:nth-child(3) .a-text-bold').css('::text').extract(),
+                memory_speed = i.css('.puis-padding-right-small:nth-child(4) .a-text-bold').css('::text').extract()
             )
 
             yield item
